44336f46cf6e861e5ee39ce4c860e183fdcf8d52
Daniel <danielh839@gmail.com>
2021-09-19 02:39:11 -0400

    Renamed

--- puzzles/filter.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('lichess.csv')
-
-df = df[(df['Result'] == '1-0') | (df['Result'] == '0-1')]
-df['Elo'] = df['BlackElo'] + df['WhiteElo'] / 2
-df = df.sort_values(by='Elo', ascending=False)
-
-df[:5000].to_csv('lichess_5000.csv')
